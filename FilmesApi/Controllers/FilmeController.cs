14efd4d9e61ef5e8c44f4d7ffac6018f407f7453
Magno Moreira <magnomoreira@yahoo.com>
2022-02-22 08:20:26 -0300

    Created method Post end Get

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,28 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace FilmesApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class FilmeController : ControllerBase
+    {
+        private readonly List<Filme> _filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionarFilme([FromBody]Filme filme)
+        {
+            _filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Filme> RecuperaFilme()
+        {
+            return _filmes;
+        }
+    }
+}
---
+++ FilmesApi/Models/Filme.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models
+{
+    public class Filme
+    {
+        [Required(ErrorMessage = "O campo titulo é obrigatório")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo diretor é obrigatório")]
+        public string Diretor { get; set; }
+        public string Genero { get; set; }
+        [Range(1, 600, ErrorMessage = "A duração deve ter no minimo 1 minuto e no maxímo 600 ")]
+        public int DuracaoEmMinutos { get; set; }
+
+    }
+}
