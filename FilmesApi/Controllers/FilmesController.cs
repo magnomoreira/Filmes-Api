9122620e694582349dd9afbaaddfc66f420217d8
Magno Moreira <magnomoreira@yahoo.com>
2022-02-22 07:49:17 -0300

    Creating project FilmesApi

---
+++ FilmesApi/Controllers/FilmesController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class FilmesController : ControllerBase
+    {
+    }
+}
