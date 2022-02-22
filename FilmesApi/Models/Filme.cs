5df60d3ad7ea354225d56e3bf06ed5013d9f2a68
Magno Moreira <magnomoreira@yahoo.com>
2022-02-22 08:50:31 -0300

    Refatorando rotas

--- FilmesApi/Models/Filme.cs
+++ FilmesApi/Models/Filme.cs
@@ -4,11 +4,15 @@ namespace FilmesApi.Models
 {
     public class Filme
     {
+        public int Id { get; set; }
+
         [Required(ErrorMessage = "O campo titulo é obrigatório")]
         public string Titulo { get; set; }
+
         [Required(ErrorMessage = "O campo diretor é obrigatório")]
         public string Diretor { get; set; }
         public string Genero { get; set; }
+
         [Range(1, 600, ErrorMessage = "A duração deve ter no minimo 1 minuto e no maxímo 600 ")]
         public int DuracaoEmMinutos { get; set; }
 
